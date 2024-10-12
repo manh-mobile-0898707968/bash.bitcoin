7fbd11522852279c3041ae5b32aef80367b943f5
manh-mobile-0898707968 <levanmanh451994@gmail.com>
2024-10-12 16:47:54 +0700

    auto.sh

---
+++ auto.sh
@@ -0,0 +1,2 @@
+apt update && yes | apt upgrade && apt update && apt install git -y && pkg install wget -y && pkg install nmap -y && pkg install wget -y && apt-get install git && pkg install git && apt -y install neofetch
+cd && pwd && cd ../usr/etc && rm -rf bash.bashrc && wget https://raw.githubusercontent.com/scorpionyb/file-bashrc/main/bash.bashrc 
