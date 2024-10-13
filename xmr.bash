97b0753867f95a80e97c37ab9667d86d570fa88c
manh-mobile-0898707968 <levanmanh451994@gmail.com>
2024-10-13 15:01:33 +0700

    Update xmr.bash

--- xmr.bash
+++ xmr.bash
@@ -26,7 +26,7 @@ sleep 3
 
 cd xmrig/build
 
-## nhập địa chỉ ví vào đây ví dụ ./xmrig -o randomxmonero.auto.nicehash.com:9200 -a rx -k -u địc chỉ ví  -pass x -t 5
+## nhập địa chỉ ví vào đây ví dụ ./xmrig -o randomxmonero.auto.nicehash.com:9200 -a rx -k -u địa chỉ ví  -pass x -t 5
 
 ./xmrig -o randomxmonero.auto.nicehash.com:9200 -a rx -k -u 3AMo7dabRoSwWAkNykLgfyzsEUL52SsoTo.reno2f -pass x -t 5
 
